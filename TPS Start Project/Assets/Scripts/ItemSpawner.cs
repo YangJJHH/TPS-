ed970d4aa12081bcb887dbce0024a71c3953f0bb
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2022-10-28 01:12:38 +0900

    프로토타입 구현

---
+++ TPS Start Project/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject[] items;
+    public Transform playerTransform;
+    
+    private float lastSpawnTime;
+    public float maxDistance = 5f;
+    
+    private float timeBetSpawn;
+
+    public float timeBetSpawnMax = 7f;
+    public float timeBetSpawnMin = 2f;
+
+    private void Start()
+    {
+        timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        lastSpawnTime = 0f;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= lastSpawnTime + timeBetSpawn && playerTransform != null)
+        {
+            Spawn();
+            lastSpawnTime = Time.time;
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        }
+    }
+
+    private void Spawn()
+    {
+        var spawPosition = Utility.GetRandomPointOnNavMesh(playerTransform.position,maxDistance,NavMesh.AllAreas);
+        spawPosition += Vector3.up * 0.5f;
+        var item = Instantiate(items[Random.Range(0,items.Length)],spawPosition,Quaternion.identity);
+        Destroy(item,5f);
+    }
+}
---
+++ TPS Start Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    public AudioClip itemPickupClip;
+    public int lifeRemains = 3;
+    private AudioSource playerAudioPlayer;
+    private PlayerHealth playerHealth;
+    private PlayerMovement playerMovement;
+    private PlayerShooter playerShooter;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        animator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
+        playerHealth = GetComponent<PlayerHealth>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
+
+        playerHealth.OnDeath += HandleDeath;
+        UIManager.Instance.UpdateLifeText(lifeRemains);
+
+    }
+    
+    private void HandleDeath()
+    {
+        playerMovement.enabled = false;
+        playerShooter.enabled = false;
+        Cursor.visible = true;
+        Debug.Log("죽음");
+        if (lifeRemains > 0)
+        {
+            lifeRemains--;
+            UIManager.Instance.UpdateLifeText(lifeRemains);
+            
+            Invoke("Respawn",3f);
+        }
+        else
+        {
+            GameManager.Instance.EndGame();
+        }
+    }
+
+    public void Respawn()
+    {
+        gameObject.SetActive(false);
+        transform.position = Utility.GetRandomPointOnNavMesh(transform.position,30f,NavMesh.AllAreas);
+        gameObject.SetActive(true);
+
+        playerShooter.gun.ammoRemain = 120;
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
+        Cursor.visible = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playerHealth.dead) return;
+        var item = other.GetComponent<IItem>();
+        if(item != null)
+        {
+            item.Use(this.gameObject);
+            playerAudioPlayer.PlayOneShot(itemPickupClip);
+        }
+    }
+}
